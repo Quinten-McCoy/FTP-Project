93d5fd6c810a70e91a5f75dadbad1d01c7cfb9e3
Quinten-McCoy <114446499+Quinten-McCoy@users.noreply.github.com>
2022-11-21 18:42:03 -0600

    Server Menu with verification
    New menu

--- serverMenu.py
+++ serverMenu.py
@@ -1,83 +1,103 @@
-import threading
 import socket
-from unittest import skip
+import os
 
-HEADER = 1024
+HEADER = 4096
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 DISCONNECT_MESSEGE = '!DISCONNECT'
+SEPARATOR = "<SEPARATOR>"
+
+#Creating users
+admin_dict = {
+    #'dn': 'cn=admin,dc=filetransfer,dc=com',
+    'cn': 'admin',
+    'description': 'LDAP Admin',
+    'objectClass': 'simpleSecurityObject',
+    'pass': 'Access123',
+}
+
+userone_dict = {
+    #'dn': 'user one,dc=filetransfer,dc=com',
+    'cn': 'userone',
+    'description': 'Example user',
+    'objectclass': 'inetOrgPerson',
+    'pass': 'pass'
+}
 
 #Creating Server
 print(f"[SERVER] Starting...")
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def main():
-    #selection = input('1: Connect User\n2: Exit\n')
-    selection = '1'
-    while selection != '2':
-        if selection == '1':
-            connectUser()
-            #selection = input('1: Connect User\n2: Exit\n')
-    return 0
-
-
-def authenticateUser():
-    pass
-
-def createUser():
-    pass
-
 def connectUser():
+    #Listening on...
     print(f"[SERVER] Listening on {ADDR}")
     server.listen(5)
 
-    while True:
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION {addr} connected")
-        clientThread = threading.Thread(target=clientHandling, args = (conn, addr))
-        clientThread.start()
-        #Displaying number of connections to server
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1 }")
+    #connect users
+    client_socket, addr = server.accept()
+    print(f"[NEW CONNECTION {addr} connected")
+    clientHandling(client_socket, addr)
 
-def clientHandling(conn, addr):
-    #Deals with clients
-    selection = conn.recv(HEADER).decode()
-    print(selection)
+def clientHandling(client_socket, addr):
+    
+    msg = client_socket.recv(HEADER).decode()
+    cn, password  = msg.split('<SEPERATOR>')
 
-    while selection != DISCONNECT_MESSEGE:
-        connected = True
-        while connected == True:
-            if selection == '1':
-                #Preparing to write data to file
-                print("[SERVER] Recieving Data")
+    user_level, can_logon = verify_creds(cn, password) 
+    
+    if can_logon == True:
+        selection = '1'
 
-                #Opening file/recieving & writing data
-                file = open("incommingFile", 'w')
-                fileData = conn.recv(HEADER).decode()
-                file.write(fileData)
-                file.close()
+        while selection != DISCONNECT_MESSEGE:
+            connected = True
+            while connected == True:
+                if selection == '1':
+                    #FILE UPLOAD
+                    #Preparing to write data to file
+                    print("[SERVER] Recieving Data")
 
-                #Disconnecting client
-                connected = False
+                    #Getting name and size of file
+                    msg = client_socket.recv(HEADER).decode()
+                    fileName, fileSize = msg.split('<SEPERATOR>')
+                    print(fileName, fileSize)
 
-            elif selection == '2':
-                print("Sending File (Unfinished")
 
-            connected = False
+                    with open('Recieved', 'wb') as f:
+                        contents = client_socket.recv(HEADER).decode()   
+                        f.write(contents)
+
+                    selection = DISCONNECT_MESSEGE            
+
+                elif selection == '2':
+                    print("Sending File (Unfinished")
+
+                    connected = False
+
         print(f"[SERVER] Client {addr} has disconnected")
-
-    conn.close()
-
+        client_socket.close()
 
 
+def verify_creds(cn, password):
+    new_dict = {
+        'cn': cn,
+        'pass': password,
+    }
 
-
+    if new_dict['cn'] == userone_dict['cn'] and new_dict['pass'] == userone_dict['pass']:
+        print('Logged in as Userone')
+        level, logon = 'user', True
+        return(level, logon)
+    elif new_dict['cn'] == admin_dict['cn'] and new_dict['pass'] == admin_dict['pass']:
+        print('Logged in as Admin')
+        level, logon = 'admin', True
+        return(level, logon)
+    else:
+        level, logon = 'none', False
+        return(level, logon)
 
     
 
-    
-
-main()
+connectUser()
 
